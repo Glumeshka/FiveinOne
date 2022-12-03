acd59f94b1c8cf769252d10151f660b567fa6e8c
Glumeshka <rzl1985@gmail.com>
2022-12-03 04:56:32 +0300

    no bad idea

--- task3/script.js
+++ task3/script.js
@@ -2,10 +2,17 @@ const trafficLight = document.querySelectorAll('.trafficLight');
 let activeLight = 1;
 
 
-document.querySelector('.trafficLight').addEventListener('click', (e) => {
+document.querySelector('#one').addEventListener('click', (e) => {
     changeLight()
 })
 
+document.querySelector('#two').addEventListener('click', (e) => {
+    changeLight()
+})
+
+document.querySelector('#tree').addEventListener('click', (e) => {
+    changeLight()
+})
 
 function changeLight() {
     trafficLight[activeLight].className = 'trafficLight';
